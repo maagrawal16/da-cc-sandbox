7b24698c20eb1135fc723cac29bbc6daeb641e56
Nishant Kumar Thakur <nishant.thakur.ece@gmail.com>
2025-04-08 00:41:30 +0530

    Pointing to ms-apps hosted in "milo" repo

--- tools/ms-apps/daUtils.js
+++ tools/ms-apps/daUtils.js
@@ -6,8 +6,18 @@ import DA_SDK from 'https://da.live/nx/utils/sdk.js';
     const { context, token } = await DA_SDK;
     const { repo, path, ref } = context;
     const redirectPath = path.split('/').pop();
+
+    let appHost = 'https://milostudio--milo--adobecom.aem.page';
+    switch (ref) {
+      case 'stage':
+        appHost = 'https://milostudio-stage--milo--adobecom.aem.page';
+        break;
+      case 'dev':
+      case 'local':
+        appHost = 'https://milostudio-dev--milo--adobecom.aem.page';
+    }
     window.location.replace(
-      `../da-apps/ms-apps.html?path=${redirectPath}&tenant=${repo}&ref=${ref}&token=${token}`
+      `${appHost}/tools/da-apps/ms-apps.html?path=${redirectPath}&tenant=${repo}&ref=${ref}&token=${token}`
     );
   } catch (error) {
     console.error('Error initializing DA_SDK:', error);
